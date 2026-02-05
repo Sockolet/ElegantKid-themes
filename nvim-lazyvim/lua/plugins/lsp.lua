6339a2e6f145e92752853dcf9bcded51cee170cf
Sockolet <adrianreysanchez@outlook.com>
2026-02-05 19:13:00 +0100

    Fix mason-lspconfig setup_handlers error
    Use handlers option inside setup() instead of setup_handlers()

--- nvim-lazyvim/lua/plugins/lsp.lua
+++ nvim-lazyvim/lua/plugins/lsp.lua
@@ -26,31 +26,30 @@ return {
           "pyright",
           "tsserver",
         },
-      })
-
-      mason_lspconfig.setup_handlers({
-        function(server_name)
-          lspconfig[server_name].setup({
-            capabilities = capabilities,
-          })
-        end,
-        ["lua_ls"] = function()
-          lspconfig.lua_ls.setup({
-            capabilities = capabilities,
-            settings = {
-              Lua = {
-                diagnostics = {
-                  globals = { "vim" },
+        handlers = {
+          function(server_name)
+            lspconfig[server_name].setup({
+              capabilities = capabilities,
+            })
+          end,
+          ["lua_ls"] = function()
+            lspconfig.lua_ls.setup({
+              capabilities = capabilities,
+              settings = {
+                Lua = {
+                  diagnostics = {
+                    globals = { "vim" },
+                  },
+                  workspace = {
+                    library = vim.api.nvim_get_runtime_file("", true),
+                    checkThirdParty = false,
+                  },
+                  telemetry = { enable = false },
                 },
-                workspace = {
-                  library = vim.api.nvim_get_runtime_file("", true),
-                  checkThirdParty = false,
-                },
-                telemetry = { enable = false },
               },
-            },
-          })
-        end,
+            })
+          end,
+        },
       })
 
       -- Keymaps
